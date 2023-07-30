7e67bb0c0ea55c0bff6db49512ceaf9e5e02e01f
SDW <stephenoilesanmi@gmail.com>
2023-07-30 18:28:56 +0100

    added comments for app.py

--- app.py
+++ app.py
@@ -15,11 +15,16 @@ from assets.made_by_sdw import made_by_sdw
 
 if __name__ == "__main__":
 
+    ########### A. SIDEBAR ###########
+
     # Prepare data for the sidebar dropdowns
     sidebar_data = prepare_sidebar_data(database_schema_dict)
     st.sidebar.markdown("<div class='made_by'>Made by SDW🔋</div>", unsafe_allow_html=True)
 
     
+
+    ### POSTGRES DB OBJECTS VIEWER ###
+
     st.markdown(made_by_sdw, unsafe_allow_html=True)
     st.sidebar.title("🔍 Postgres DB Objects Viewer")
 
@@ -39,22 +44,36 @@ if __name__ == "__main__":
 
 
 
-    # Retrieve the current theme from session state
-    current_theme = st.session_state.get("theme", "light")
-    st.markdown(f"<body class='{current_theme}'></body>", unsafe_allow_html=True)
 
 
+    ### SAVE CONVERSATION BUTTON ###
+
+    # Add a button to SAVE the chat/conversation
     if st.sidebar.button("Save Conversation💾"):
         saved_file_path = save_conversation(st.session_state["full_chat_history"])
         st.sidebar.success(f"Conversation saved to: {saved_file_path}")
         st.sidebar.markdown(f"Conversation saved! [Open File]({saved_file_path})")
 
+    
+    
+    
 
-    # Add a button to clear the chat/conversation
+    ### CLEAR CONVERSATION BUTTON ###
+
+    # Add a button to CLEAR the chat/conversation
     if st.sidebar.button("Clear Conversation🗑️"):
         save_conversation(st.session_state["full_chat_history"]) 
         clear_chat_history()
 
+
+
+
+
+    ### TOGGLE THEME BUTTON ###
+
+    # Retrieve the current theme from session state
+    current_theme = st.session_state.get("theme", "light")
+    st.markdown(f"<body class='{current_theme}'></body>", unsafe_allow_html=True)
 
 
     # Initialize the theme in session state
@@ -62,8 +81,7 @@ if __name__ == "__main__":
         st.session_state.theme = "light"
 
 
-
-    # Toggle theme on button click
+    # Add a button to toggle the UI colour theme
     if st.sidebar.button("Toggle Theme🚨"):
         st.session_state.theme = "dark" if st.session_state.theme == "light" else "light"
         st.experimental_rerun()
@@ -74,10 +92,28 @@ if __name__ == "__main__":
     theme_style = dark if st.session_state.theme == "dark" else light
     st.markdown(theme_style, unsafe_allow_html=True)
 
+
+
+
+
+
+
+
+
+
+
+    ########### B. CHAT INTERFACE ###########
+
+
+    
+    ### TITLE ###
+
     # Add title to the Streamlit chatbot app
     st.title("🤖 AI Database Chatbot 🤓")
 
 
+
+    ### SESSION STATE ###
 
     # Initialize the full chat messages history for UI
     if "full_chat_history" not in st.session_state:
@@ -91,6 +127,9 @@ if __name__ == "__main__":
 
 
 
+    ### CHAT FACILITATION ###
+
+    # Start the chat
     if (prompt := st.chat_input("What do you want to know?")) is not None:
         st.session_state.full_chat_history.append({"role": "user", "content": prompt})
 
